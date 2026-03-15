741212bc8598210994ca6948ce58d68c5885265e
Tung Pham <tung.42@gmail.com>
2026-03-15 12:46:55 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,6 +31,11 @@ class Post extends Model
         ]);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
