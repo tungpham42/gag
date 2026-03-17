984fe32ae31f5e7bfc48fecf9fc8e12dd96b1b41
Tung Pham <tung.42@gmail.com>
2026-03-17 09:45:44 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,6 +109,8 @@
     <script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.0.2/js/dataTables.tailwindcss.js"></script>
 
+    <script src="https://platform.soft.io.vn/wp-json/saas/v1/embed.js?api_key=sk_live_LnFaKwLoGUiBTMtZ4Bef8C5j" defer></script>
+
     @stack('scripts')
     @guest
         <script>
